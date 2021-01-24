57d8f4f6547989d2cb877e366382e4f9a3dbb7be
globnob <blueredblack@windowslive.com>
2021-01-25 00:26:09 +0100

    master

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <h1 class="header1">
+      Todolist
+    </h1>
+  );
+}
+
+export default App;
